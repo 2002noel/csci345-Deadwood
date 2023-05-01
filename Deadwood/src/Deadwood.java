7ff3fcd0d6d62283e1471f1b0058ddccc2036daa
Noel <kimn20@wwu.edu>
2023-05-01 01:09:44 -0700

    deleted class files

--- Deadwood/src/Deadwood.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-import java.io.*;
-import Board;
-
-
-public class Deadwood {
-}
---
+++ Deadwood/src/classes/Deadwood.java
@@ -0,0 +1,8 @@
+package classes;
+import java.util.*;
+import classes.Player;
+import java.io.*;
+
+public class Deadwood {
+    private Player[] players;
+}
