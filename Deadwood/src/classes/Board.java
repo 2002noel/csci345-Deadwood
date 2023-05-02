cd7cac9e87a30177c6ee75496e0ed5f04b870c41
Noel <kimn20@wwu.edu>
2023-05-01 23:18:22 -0700

    updated

--- Deadwood/src/classes/Board.java
+++ Deadwood/src/classes/Board.java
@@ -4,6 +4,6 @@ package classes;
 public class Board{
     private Set[] sets = new Set[10];
     private Player[] players;
-    private String[] location;
+    private int[] location;
     private int scenesLeft;
 }
