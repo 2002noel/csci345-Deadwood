6ac81da2fcf361c219eecbd9688275caf3de46c4
Noel <kimn20@wwu.edu>
2023-05-01 22:39:16 -0700

    idk what i changed

--- Deadwood/src/classes/Board.java
+++ Deadwood/src/classes/Board.java
@@ -5,6 +5,6 @@ import javax.print.attribute.SetOfIntegerSyntax;
 public class Board{
     private Set[] sets = new Set[10];
     private Player[] players;
-    //create a data structure to show the locations of the players on which set
-    
+    private String[] location;
+    private int scenesLeft;
 }
