1922baf07469b38d6c3bd00dc053bd75d6753233
Noel <kimn20@wwu.edu>
2023-05-01 22:05:39 -0700

    added some stuff

--- Deadwood/src/classes/Board.java
+++ Deadwood/src/classes/Board.java
@@ -1,5 +1,10 @@
 package classes;
 
+import javax.print.attribute.SetOfIntegerSyntax;
+
 public class Board{
+    private Set[] sets = new Set[10];
+    private Player[] players;
+    //create a data structure to show the locations of the players on which set
     
 }
