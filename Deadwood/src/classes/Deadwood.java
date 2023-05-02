e4b8a64d9e7906e834b0fd79f06fd8d0ce5306fe
Noel <kimn20@wwu.edu>
2023-05-01 21:43:34 -0700

    did set and game

--- Deadwood/src/classes/Deadwood.java
+++ Deadwood/src/classes/Deadwood.java
@@ -7,4 +7,28 @@ public class Deadwood {
     private Player[] players;
     private int day;
     public Die die;
+    public Board board;
+
+
+    public static void newDay(){
+        return;
+    }
+
+    public static void endDay(){
+        return;
+    }
+
+    public static void endGame(){
+        return;
+    }
+
+    public static void upgradeRank(){
+        return;
+    }
+
+    public static void startGame(){
+        return;
+    }
 }
+
+
--- Deadwood/src/classes/Set.java
+++ Deadwood/src/classes/Set.java
@@ -1,3 +1,24 @@
 package classes;
 
-public class Set {}
+public class Set {
+
+    private int shotsleft;
+    public Scene scene;
+    private String name;
+    private Set adjacentSet;
+    private Roles[] roles;
+
+
+    public static void finishScene(){
+        return;
+    }
+
+    public static void finishShot(){
+        return;
+    }
+
+    private static void payout(){
+        return;
+    }
+
+}
