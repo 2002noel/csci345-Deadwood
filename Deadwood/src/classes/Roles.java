7d0e085c6c4befd57c8393b2ca485a575b255d69
Noel <kimn20@wwu.edu>
2023-05-08 23:57:47 -0700

    setter and getter

--- Deadwood/src/classes/Roles.java
+++ Deadwood/src/classes/Roles.java
@@ -1,6 +1,25 @@
 package classes;
 
 public class Roles{
-    public int rank;
-    public String name;
+    private int rank;
+    private String name;
+
+
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setRank(int rank){
+        this.rank = rank;
+    }
+
+    public int getRank(){
+        return rank;
+    }
+    
 }
