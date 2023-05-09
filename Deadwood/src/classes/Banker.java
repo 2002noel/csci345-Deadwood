2124fddf22e6a5b3f69450b752631575ebb06814
Noel <kimn20@wwu.edu>
2023-05-08 23:50:21 -0700

    banker class

--- Deadwood/src/classes/Banker.java
+++ Deadwood/src/classes/Banker.java
@@ -1,11 +1,14 @@
 package classes;
 
 public class Banker extends Player {
-    private int bankFunds;
     
-    public void changeFunds(Player ply, int funds) {
+    public void transferfunds(Player ply, int amount) {
+        ply.addCredits(amount);
     }
-    
-    public void upgradePlayer(Player ply, int rank) {
+
+    public void givemoneytobank(Player ply, int amount) {
+        ply.removeMoney(amount);
     }
+
 }
+    
