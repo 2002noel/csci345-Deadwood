96b83883b1abcf6b553f0f5048d053fb1e486e35
Noel <kimn20@wwu.edu>
2023-05-13 22:42:46 -0700

    added in getter and setter for Deadwood

--- Deadwood/src/classes/Deadwood.java
+++ Deadwood/src/classes/Deadwood.java
@@ -12,25 +12,57 @@ public class Deadwood {
 
 
 
-    public static void newDay(){
-        return;
+    public Deadwood() {
+        this.day = 1;
+        this.die = new Die();
+        this.board = new Board();
+        this.banker = new Banker();
     }
 
-    public static void endDay(){
-        return;
+    public int getDay() {
+        return day;
     }
 
-    public static void endGame(){
-        return;
+    public void incrementDay() {
+        day++;
     }
 
-    public static void upgradeRank(Player ply) {
-        return;
+    public void setPlayers(Player[] players) {
+        this.players = players;
     }
 
-    public static void startGame(){
-        return;
+    public Player[] getPlayers() {
+        return players;
     }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setDie(Die die) {
+        this.die = die;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public void setBanker(Banker banker) {
+        this.banker = banker;
+    }
+
+    public Die getDie() {
+        return die;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Banker getBanker() {
+        return banker;
+    }
+
 }
 
 
