e3d2e6c1fe42097ed90fbf477f4a8e8f8dfeb868
Noel <kimn20@wwu.edu>
2023-05-16 01:14:41 -0700

    player and banker set up

--- Deadwood/src/Deadwood.java
+++ Deadwood/src/Deadwood.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import classes.Player;
 import java.io.*;
 import classes.*;
 
@@ -16,6 +15,28 @@ public class Deadwood {
         //create a new game
 
         Systems game = Systems.getInstance();
+
+        //assign a random player to be the banker
+        Random rand = new Random();
+        int banker = rand.nextInt(numPlayers);
+        //create an array of players
+        Player[] players = new Player[numPlayers];
+        //create the players, except for the banker index, then create a banker
+        for( int i = 0; i < numPlayers; i++){
+            if(i != banker){
+                players[i] = new Player(0, 1);
+            }
+            else{
+                players[i] = new Banker(0, 1);
+                //set the banker in the game
+                game.setBanker((Banker)players[i]);
+                //print out the player num that is the banker
+                System.out.println("Player " + (i+1) + " is the banker");
+            }
+        }
+
+        //set the players in the game
+        game.setPlayers(players);
 
 
 
