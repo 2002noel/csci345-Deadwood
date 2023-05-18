cc17780f20041218937dafd027da733fd9b1eb33
Ben Thompson <ben.t11@hotmail.com>
2023-05-18 11:13:54 -0700

    use command line arg for numplayers and start players with correct handicap

--- Deadwood/src/Deadwood.java
+++ Deadwood/src/Deadwood.java
@@ -9,10 +9,20 @@ public class Deadwood {
         //create a new game
         int numPlayers = 0;
         //ask for number of players
-        System.out.println("How many players are playing?");
         Scanner sc = new Scanner(System.in);
-        numPlayers = sc.nextInt();
-        sc.nextLine();
+        if (args.length < 1) {
+            System.out.println("How many players are playing?");
+            numPlayers = sc.nextInt();
+            sc.nextLine();
+        } else {
+            numPlayers = Integer.parseInt(args[0]);
+        }
+
+        if (numPlayers < 2 || numPlayers > 8) {
+            System.err.println("Invalid number of players");
+            return;
+        }
+
         Systems game = Systems.getInstance();
 
         //assign a random player to be the banker
@@ -20,14 +30,22 @@ public class Deadwood {
         int banker = rand.nextInt(numPlayers);
         //create an array of players
         Player[] players = new Player[numPlayers];
+        int startingCredits = 0;
+        int startingRank = 1;
+        if (numPlayers == 5) {
+            startingCredits = 2;
+        } else if (numPlayers == 6) {
+            startingCredits = 4;
+        } else if (numPlayers > 6) {
+            startingRank = 2;
+        }
         //create the players, except for the banker index, then create a banker
-        for( int i = 0; i < numPlayers; i++){
-            if(i != banker){
-                players[i] = new Player(0, 1);
+        for (int i = 0; i < numPlayers; i++) {
+            if(i != banker) {
+                players[i] = new Player(startingCredits, startingRank);
                 players[i].setid(i+1);
-            }
-            else{
-                players[i] = new Banker(0, 1);
+            } else {
+                players[i] = new Banker(startingCredits, startingRank);
                 //set the banker in the game
                 game.setBanker((Banker)players[i]);
                 players[i].setid(i+1);
@@ -54,10 +72,10 @@ public class Deadwood {
         
         
         int lastday = 4;
-        if(numPlayers < 3){
+        if(numPlayers < 4){
             lastday = 3;
         }
-        //
+
         while(game.getDay() < lastday){
             //start the day
             game.startDay();
